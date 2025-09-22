df052f7dde2e07a704c95956bb7afe9429ab767e
Chaitanya Datar <datar.chaitanya@gmail.com>
2025-09-22 21:15:04 +0530

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findCenter(int[][] edges) {
+        int a = 0;
+        int b = 0;
+        a = edges[0][0];
+        b = edges[0][1];
+
+        if(a == edges[1][0]){
+            return(a);
+        }
+
+        if(a == edges[1][1]){
+            return(a);
+        }
+
+        return(b);
+    }
+}
